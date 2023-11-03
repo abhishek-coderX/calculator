b00675e8c77d52e4732ce02e4629d9e0891f1544
abhishek-coderX <abhipal2330@gmail.com>
2023-11-03 20:27:04 +0530

    Your commit message here

---
+++ app.js
@@ -0,0 +1,24 @@
+let inputBox = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+let string = '';
+
+buttons.forEach(element => {
+    element.addEventListener('click', (b) => {
+        if (b.target.innerText === '=') {
+            string = String(eval(string));
+            inputBox.value = string;
+        } else if (b.target.innerText === 'Ac') {
+            string = '';
+            inputBox.value = string;
+        } else if (b.target.innerText === 'Del') {
+            string = string.substring(0, string.length - 1);
+            inputBox.value = string;
+        } else if (b.target.id === 'plusMinus') {
+            string = String(-eval(string));
+            inputBox.value = string;
+        } else {
+            string += b.target.innerText;
+            inputBox.value = string;
+        }
+    });
+});
